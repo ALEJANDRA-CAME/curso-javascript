da419dc898ead5288ac45c2ed47a70ff6e389db9
ALEJANDRA-CAME <camelo.alejandra@hotmail.com>
2024-11-05 22:37:27 -0500

    Add files via upload

---
+++ nombre.js
@@ -0,0 +1,23 @@
+/ respuesta 1
+persona = {
+    "nombre": "Alejandra" ,
+    "apellido": "Camelo" ,
+    "edad": "28" ,
+    "idiomas": "español-inglés" ,
+    "comida favorita": "hamburguesas" ,
+    "lugar favorito": "rodearme con naturaleza" ,
+    "musica favorita": "el pop especialmente Andrés Cepeda"
+}
+
+console.log(persona.nombre)
+
+/respuesta 2
+fecha = prompt("ingresa en que mes estamos:")
+console.log("el mes en el que estamos es:" , fecha)
+alert("Estas en el mes de " + fecha)
+
+/repuesta 3
+edad = prompt("ingresa tu edad:")
+año = 2024
+nacimiento = (edad-año)
+alert("Tu naciste aproximadamente en el año" + nacimiento)
